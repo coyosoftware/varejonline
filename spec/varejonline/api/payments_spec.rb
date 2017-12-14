4b05d7dd8d5b5f42e208c03ab791a71e64eb72e4
Coyô Dev <pedro@coyo.com.br>
2017-12-14 11:46:31 -0200

    List e find de baixas

--- spec/varejonline/api/payments_spec.rb
+++ spec/varejonline/api/payments_spec.rb
@@ -2,6 +2,43 @@ require 'spec_helper'
 
 RSpec.describe Varejonline::API::Payments do
   let(:header) { {'Content-Type' => 'application/json', 'Accept' => 'application/json'} }
+
+  describe '.list' do
+    before do
+      stub_request(:get, "#{Varejonline::API_ADDRESS}/baixas/").to_return(status: 200)
+    end
+    
+    it 'issues a get to the payments url' do
+      expect(described_class).to receive(:get).with('/', body: { token: 'abc' }.to_json, headers: header).and_call_original
+      
+      Varejonline.new('abc').payments.list()
+    end
+    
+    it 'issues a get to the payments url using the searcher' do
+      searcher = Varejonline::Searcher::Financial::PaymentSearcher.new({ quantidade: 10, inicio: 0, 
+        desde: '01-01-2015', ate: '10-01-2015', filtroData: 'dataVencimento', alteradoApos: '01-01-2015 10:10:10' })
+
+      expect(described_class).to receive(:get).with('/', body: searcher.as_parameter.merge!({ token: 'abc' }).to_json, headers: header).and_call_original
+
+      Varejonline.new('abc').payments.list(searcher)
+    end
+   
+    it 'raises ArgumentError if the parameter is not a paymentsearcher' do
+      expect{Varejonline.new('abc').payments.list(Array.new)}.to raise_error(ArgumentError)
+    end
+  end
+  
+  describe '.find' do
+    before do
+      stub_request(:get, "#{Varejonline::API_ADDRESS}/baixas/1").to_return(status: 200)
+    end
+    
+    it 'returns an especific receipt' do
+      expect(described_class).to receive(:get).with('/1', body: { token: 'abc' }.to_json, headers: header).and_call_original
+
+      Varejonline.new('abc').payments.find(1)
+    end    
+  end
   
   describe '.create' do
     before do
