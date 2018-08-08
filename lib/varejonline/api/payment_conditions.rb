dda13d1c291ad009427a6705a74164744ab382fd
Fabiana Cristina <fabianacristina89@gmail.com>
2018-08-08 16:36:11 -0300

    consulta de condições de pagamento

--- lib/varejonline/api/payment_conditions.rb
+++ lib/varejonline/api/payment_conditions.rb
@@ -11,6 +11,9 @@ module Varejonline
         return parse_response(self.class.get('/', body: build_body(searcher.as_parameter), headers: header)) if searcher
         return parse_response(self.class.get('/', body: build_body, headers: header)) unless searcher
       end
+      def find(id)
+        return parse_response(self.class.get("/#{id}", body: build_body, headers: header))
+      end
     end
   end
 end
--- spec/varejonline/api/payment_conditions_spec.rb
+++ spec/varejonline/api/payment_conditions_spec.rb
@@ -8,10 +8,21 @@ RSpec.describe Varejonline::API::PaymentConditions do
       stub_request(:get, "#{Varejonline::API_ADDRESS}/planos-pagamento/").to_return(status: 200)
     end
     
-    it 'issues a get to the product categories url' do
+    it 'issues a get to the payment conditions url' do
       expect(described_class).to receive(:get).with('/', body: { token: 'abc' }.to_json, headers: header).and_call_original
       
       Varejonline.new('abc').payment_conditions.list
+    end
+  end
+  describe '.find' do
+    before do
+      stub_request(:get, "#{Varejonline::API_ADDRESS}/planos-pagamento/5").to_return(status: 200)
+    end
+    
+    it 'issues a get to the payment conditions url' do
+      expect(described_class).to receive(:get).with('/5', body: { token: 'abc' }.to_json, headers: header).and_call_original
+      
+      Varejonline.new('abc').payment_conditions.find(5)
     end
   end
 end
